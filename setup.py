cf617f8dd833788e9f3a4b4e09521097ca90de73
Brightcells <brightcells@gmail.com>
2018-03-30 14:09:39 +0800

    :art: Add install_requires pywe-sign>=1.0.8

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
 
     packages=['pywe_decrypt'],
     py_modules=[],
-    install_requires=['pycrypto', 'pywe-xml>=1.0.2'],
+    install_requires=['pycrypto', 'pywe-sign>=1.0.8', 'pywe-xml>=1.0.2'],
 
     classifiers=[
         "License :: OSI Approved :: BSD License",
